edac2ef75aadf207eddc15148ab3ddd008181702
Stephane Nicoll <snicoll@pivotal.io>
2016-05-11 17:48:56 +0200

    Caching demonstration
    Our github integration could quickly reach the 60 calls per minute rate
    limit. We can actually visualize the issue by exporting the metrics to
    a different system and monitor the `cfp.github.requets` metric. One way
    to do that would be to add the following bean definition:
    
    ```
    @Bean
    @ExportMetricWriter
    public MetricWriter metricWriter(MBeanExporter exporter) {
      return new JmxMetricWriter(exporter);
    }
    ```
    
    And look in JConsole. We can easily cache those results using the cache
    abstraction. This commit uses ehcache3, a JSR-107 compliant cache manager
    but many more options are available.

--- src/main/java/com/example/cfp/CfpApplication.java
+++ src/main/java/com/example/cfp/CfpApplication.java
@@ -4,9 +4,15 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
@@ -19,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CfpProperties.class)
+@EnableCaching
 public class CfpApplication {
 
 	public static void main(String[] args) {
@@ -31,6 +38,22 @@ public class CfpApplication {
 		restTemplate.setInterceptors(Collections.singletonList(
 				new GithubAppTokenInterceptor(properties.getGithub().getToken())));
 		return restTemplate;
+	}
+
+	@Bean
+	public JCacheManagerCustomizer cacheManagerCustomizer() {
+		return cm -> {
+			cm.createCache("github.commits", initConfiguration(Duration.TEN_MINUTES));
+			cm.createCache("github.polishCommit", initConfiguration(Duration.ONE_HOUR));
+			cm.createCache("github.user", initConfiguration(Duration.ONE_HOUR));
+		};
+	}
+
+	private MutableConfiguration<Object, Object> initConfiguration(Duration duration) {
+		return new MutableConfiguration<>()
+				.setStoreByValue(false)
+				.setStatisticsEnabled(true)
+				.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
 	}
 
 	private static class GithubAppTokenInterceptor implements ClientHttpRequestInterceptor {
